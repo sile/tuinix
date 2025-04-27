9a3c505cf6eb42d837ae6899596a6f6319ab14e2
Takeru Ohta <phjgt308@gmail.com>
2025-04-27 12:14:29 +0900

    feat: Replace String with BTreeMap to support styled terminal characters

--- src/frame.rs
+++ src/frame.rs
@@ -1,18 +1,24 @@
-use crate::{TerminalPosition, TerminalSize};
+use std::collections::BTreeMap;
+
+use crate::{TerminalPosition, TerminalSize, TerminalStyle};
 
 #[derive(Debug, Default, Clone)]
 pub struct TerminalFrame {
     size: TerminalSize,
-    data: String,
+    data: BTreeMap<TerminalPosition, TerminalChar>,
     tail: TerminalPosition,
+    current_style: TerminalStyle,
+    escape_sequence: String,
 }
 
 impl TerminalFrame {
     pub fn new(size: TerminalSize) -> Self {
         Self {
             size,
-            data: String::new(),
+            data: BTreeMap::new(),
             tail: TerminalPosition::ZERO,
+            current_style: TerminalStyle::new(),
+            escape_sequence: String::new(),
         }
     }
 
@@ -21,10 +27,11 @@ impl TerminalFrame {
     }
 
     pub(crate) fn lines(&self) -> impl '_ + Iterator<Item = &str> {
-        self.data
-            .lines()
-            .chain(std::iter::repeat(""))
-            .take(self.size.rows)
+        // self.data
+        //     .lines()
+        //     .chain(std::iter::repeat(""))
+        //     .take(self.size.rows)
+        std::iter::empty()
     }
 
     // TODO: merge or draw_frame
@@ -37,7 +44,20 @@ impl std::fmt::Write for TerminalFrame {
         }
 
         for c in s.chars() {
-            if c == '\n' {
+            if !self.escape_sequence.is_empty() {
+                self.escape_sequence.push(c);
+                if c.is_ascii_alphabetic() {
+                    self.current_style = self
+                        .escape_sequence
+                        .parse()
+                        .expect("escape sequence should be generated via `TerminalStyle`");
+                    self.escape_sequence.clear();
+                }
+                continue;
+            } else if c == '\x1b' {
+                self.escape_sequence.push(c);
+                continue;
+            } else if c == '\n' {
                 self.tail.row += 1;
                 self.tail.col = 0;
                 if self.tail.row >= self.size.rows {
@@ -45,11 +65,30 @@ impl std::fmt::Write for TerminalFrame {
                 }
             }
 
-            // TODO: consider cols
+            if c.is_control() {
+                continue;
+            }
 
-            self.data.push(c);
+            if self.tail.col < self.size.cols {
+                self.data.insert(
+                    self.tail,
+                    TerminalChar {
+                        style: self.current_style,
+                        value: c,
+                    },
+                );
+            }
+
+            // TODO: consider char width
+            self.tail.col += 1;
         }
 
         Ok(())
     }
+}
+
+#[derive(Debug, Clone)]
+struct TerminalChar {
+    style: TerminalStyle,
+    value: char,
 }
